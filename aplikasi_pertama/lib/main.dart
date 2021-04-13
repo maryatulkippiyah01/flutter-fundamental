067ca3e1b3f5b9450a00996e705cbf489051beb1
maryatulkippiyah <maryatulkippiyah01@gmail.com>
2021-04-13 08:48:13 +0700

    aplikasi pertama

---
+++ aplikasi_pertama/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Aplikasi Hello word"),
+      ),
+      body: Center(child: Text("Hello word")),
+    ));
+  }
+}
